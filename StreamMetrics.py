959b4f5b3f7d9c2ae0fc61232b0d3ddb7bc9f454
Nephron <38875289+Nephron00t@users.noreply.github.com>
2021-06-11 01:49:33 +1000

    Working SVM
    Working svm that uses time series data has been finished. Time series is not ideal though so will switch to some sort of feature extraction next

--- StreamMetrics.py
+++ StreamMetrics.py
@@ -1,7 +1,10 @@
+import sys
 import time
 import numpy as np
 import keyboard
+from collections import Counter
 import pandas as pd
+import pickle
 
 import brainflow
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, LogLevels, BoardIds
@@ -18,18 +21,19 @@ def main():
     if board_choice == "2":
         board_id = BoardIds.CYTON_BOARD.value
 
-    BoardShim.enable_dev_board_logger()
+    # BoardShim.enable_dev_board_logger()
     params = BrainFlowInputParams()
-    MLModel.enable_ml_logger()
+    # MLModel.enable_ml_logger()
 
     if board_id == 0:
-        #params.serial_port = "/dev/cu.usbserial-DM01MPXO"
         params.serial_port = "/dev/cu.usbserial-DM03GT61"
-        # params.serial_port = "COM4" 
+        # params.serial_port = "COM4"
 
     sampling_rate = BoardShim.get_sampling_rate(board_id)
     board = BoardShim(board_id, params)
     nfft = DataFilter.get_nearest_power_of_two(sampling_rate)
+
+    emoSVM = pickle.load(open('EmoSVM_TS_Linear.sav', 'rb'))
 
 #### PRE-PROCESSING ########################
 
@@ -37,7 +41,7 @@ def main():
         for channel in _eeg_channels:
             #5hz - 59hz bandpass
             DataFilter.perform_bandpass(_data[channel], BoardShim.get_sampling_rate(board_id), 26.5, 21.5, 4, FilterTypes.BESSEL.value, 0)
-            #50hz filter
+            # 50hz filter
             DataFilter.perform_bandstop(_data[channel], BoardShim.get_sampling_rate(board_id), 50, 1.0, 3, FilterTypes.BUTTERWORTH.value, 0)
             #Anti-wifi
             DataFilter.perform_bandstop(_data[channel], BoardShim.get_sampling_rate(board_id), 24.25, 1.0, 3, FilterTypes.BUTTERWORTH.value, 0)
@@ -53,7 +57,6 @@ def main():
     def calculate_psd(_data, _eeg_channels):
         for channel in _eeg_channels:
             DataFilter.get_psd_welch(_data[channel], nfft, nfft // 2, sampling_rate, WindowFunctions.BLACKMAN_HARRIS.value)
-            print(_data)
         return _data
 
     def get_bands(_data, _eeg_channels):
@@ -79,6 +82,39 @@ def main():
         relaxation.release()
         return relax
 
+    def get_valance(_data, _eeg_channels, _emoSVM):                                                                                                                                     
+        _df = pd.DataFrame()
+        _df = pd.DataFrame(np.transpose(_data))
+        _df = _df[_eeg_channels]
+
+        _values = _emoSVM.predict(_df)
+        _value_counts = Counter(_values)
+        pos_count = _value_counts['Positive']
+        neg_count = _value_counts['Negative']
+
+        if pos_count/10 > neg_count:
+            _valance = 'Positive'
+        else:
+            _valance = 'Negative'
+        return _valance
+
+    def get_emotion(_valance, _relax):
+        _emotion = 'null'
+        if _relax < 0.5:
+            _arousal = 'High'
+        if _relax > 0.5:
+            _arousal = 'Low'
+        
+        if _arousal == 'High' and _valance == 'Positive':
+            _emotion = "Happy"
+        if _arousal == 'High' and _valance == 'Negative':
+            _emotion = "Stessed"
+        if _arousal == 'Low' and _valance == 'Positive':
+            _emotion = 'Relaxed'
+        if _arousal == 'Low' and _valance == 'Negative':
+            _emotion = 'Sad'
+        return _emotion
+
 ##########################################        
 
     def stop_stream():
@@ -94,14 +130,19 @@ def main():
         while True:
             time.sleep(2)
             data = board.get_board_data()
+            pdData = pd.DataFrame(np.transpose(data))
 
+            data = filter_signal(data, eeg_channels)
+            # data = detrend_signal(data, eeg_channels)
             psd = calculate_psd(data, eeg_channels)
             bands = get_bands(data, eeg_channels)
 
             conc = get_concentration(bands)
             relax = get_relaxation(bands)
+            valance = get_valance(data, eeg_channels, emoSVM)
+            emotion = get_emotion(valance, relax)
 
-            print('conentration:', conc, 'relaxation:', relax)
+            print(emotion)
 
     start_stream()
 
